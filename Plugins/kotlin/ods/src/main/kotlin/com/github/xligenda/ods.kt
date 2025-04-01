28d22fcb6a4446f2a994e48c687fc72a91b47e1b
xligenda <xligenda@gmail.com>
2025-04-01 12:20:01 +0400

    patcher test

--- Plugins/kotlin/ods/src/main/kotlin/com/github/xligenda/ods.kt
+++ Plugins/kotlin/ods/src/main/kotlin/com/github/xligenda/ods.kt
@@ -3,45 +3,46 @@ package com.github.xligenda
 import android.content.Context
 import com.aliucord.annotations.AliucordPlugin
 import com.aliucord.entities.Plugin
-import com.aliucord.api.SettingsAPI
-import com.aliucord.fragments.SettingsPage
-import com.aliucord.settings.delegate
-import com.aliucord.widgets.BottomSheet
-import com.aliucord.PluginManager
-import com.aliucord.api.CommandsAPI
-import com.discord.api.commands.ApplicationCommandType
-import com.aliucord.Http
-import com.aliucord.Utils
+import com.aliucord.Logger
 import com.aliucord.patcher.before
-import com.discord.models.message.Message
 import com.discord.stores.StoreStream
-import com.discord.utilities.rest.RestAPI
-import com.discord.utilities.time.ClockFactory
-import com.discord.api.channel.Channel
-import com.discord.api.message.attachment.MessageAttachment
 import com.discord.restapi.RestAPIParams
 
-@AliucordPlugin(
-    requiresRestart = true
-)
+@AliucordPlugin(requiresRestart = true)
 class ODSClient : Plugin() {
+    private val logger = Logger("ODSClient")
+
     init {
         settingsTab = SettingsTab(PluginSettings::class.java, SettingsTab.Type.BOTTOM_SHEET).withArgs(settings)
     }
 
     override fun start(context: Context) {
-        // Patch the message sending function
         patcher.before<StoreStream>("handleSendMessage") { args ->
-            val message = args.args[1] as String
-            if (message.startsWith("!")) {
-                val capitalizedMessage = message.substring(1).uppercase()
-                args.args[1] = "!$capitalizedMessage"
+            try {
+                // Try different parameter approaches
+                val message = when {
+                    args.args.size > 1 && args.args[1] is String -> args.args[1] as String
+                    args.args[0] is Map<*, *> -> (args.args[0] as Map<*, *>)["content"] as? String
+                    else -> null
+                }
+
+                message?.let {
+                    if (it.startsWith("!")) {
+                        val modified = "!${it.substring(1).uppercase()}"
+                        if (args.args.size > 1) {
+                            args.args[1] = modified
+                        } else if (args.args[0] is Map<*, *>) {
+                            (args.args[0] as MutableMap<String, Any>)["content"] = modified
+                        }
+                    }
+                }
+            } catch (e: Exception) {
+                logger.error("Failed to modify message", e)
             }
         }
     }
 
     override fun stop(context: Context) {
-        // Remove all patches when the plugin is stopped
         patcher.unpatchAll()
     }
 }
