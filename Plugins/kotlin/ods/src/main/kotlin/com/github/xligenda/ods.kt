f41a44e225a496bf39cc7e7b0a6e679a99fc7a39
xligenda <xligenda@gmail.com>
2025-04-01 12:56:04 +0400

    patcher with uppercase

--- Plugins/kotlin/ods/src/main/kotlin/com/github/xligenda/ods.kt
+++ Plugins/kotlin/ods/src/main/kotlin/com/github/xligenda/ods.kt
@@ -3,46 +3,43 @@ package com.github.xligenda
 import android.content.Context
 import com.aliucord.annotations.AliucordPlugin
 import com.aliucord.entities.Plugin
-import com.aliucord.Logger
-import com.aliucord.patcher.before
-import com.discord.stores.StoreStream
-import com.discord.restapi.RestAPIParams
+import com.aliucord.patcher.PreHook
+import com.discord.widgets.chat.MessageContent
+import com.discord.widgets.chat.input.ChatInputViewModel
+import java.lang.reflect.Field
 
 @AliucordPlugin(requiresRestart = true)
 class ODSClient : Plugin() {
-    private val logger = Logger("ODSClient")
-
-    init {
-        settingsTab = SettingsTab(PluginSettings::class.java, SettingsTab.Type.BOTTOM_SHEET).withArgs(settings)
-    }
+    private val textContentField: Field = MessageContent::class.java
+        .getDeclaredField("textContent")
+        .apply { isAccessible = true }
 
     override fun start(context: Context) {
-        patcher.before<StoreStream>("handleSendMessage") { args ->
-            try {
-                // Try different parameter approaches
-                val message = when {
-                    args.args.size > 1 && args.args[1] is String -> args.args[1] as String
-                    args.args[0] is Map<*, *> -> (args.args[0] as Map<*, *>)["content"] as? String
-                    else -> null
-                }
+        patcher.patch(
+            ChatInputViewModel::class.java
+                .getDeclaredMethod(
+                    "sendMessage",
+                    Context::class.java,
+                    ChatInputViewModel::class.java,
+                    MessageContent::class.java,
+                    List::class.java,
+                    Boolean::class.javaPrimitiveType,
+                    Function1::class.java
+                ),
+            PreHook { callFrame ->
+                try {
+                    val messageContent = callFrame.args[2] as MessageContent? ?: return@PreHook
 
-                message?.let {
-                    if (it.startsWith("!")) {
-                        val modified = "!${it.substring(1).uppercase()}"
-                        if (args.args.size > 1) {
-                            args.args[1] = modified
-                        } else if (args.args[0] is Map<*, *>) {
-                            (args.args[0] as MutableMap<String, Any>)["content"] = modified
-                        }
-                    }
+                    val content = textContentField.get(messageContent) as String?
+                    if (content.isNullOrEmpty()) return@PreHook
+
+                    textContentField.set(messageContent, content.uppercase())
+                } catch (ignored: Throwable) {
+                    logger.error("Failed to apply CAPS LOCK", ignored)
                 }
-            } catch (e: Exception) {
-                logger.error("Failed to modify message", e)
             }
-        }
+        )
     }
 
-    override fun stop(context: Context) {
-        patcher.unpatchAll()
-    }
+    override fun stop(context: Context) = patcher.unpatchAll()
 }
